34f542a4604e06f087786375dff4895c79c54ff3
tstyer <travisstyer.ts@gmail.com>
2025-10-01 13:16:28 +0100

    build CreateUser view

--- api/views.py
+++ api/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from rest_framework import generics 
+from .serialisers import UserSerialiser
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 
 # Create your views here.
-
 def index(request):
     return HttpResponse("Working test.")
+
+class CreateUserView(generics.createAPIView):
+    queryset = User.objects.all()
+    Serialiser_class = User
+    permission_classes = [AllowAny]
