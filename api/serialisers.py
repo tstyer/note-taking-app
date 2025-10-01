f69730ef692e2a7de31df2bc8aba19cd02896517
tstyer <travisstyer.ts@gmail.com>
2025-10-01 11:54:57 +0100

    Create class and function to handle user info

--- api/serialisers.py
+++ api/serialisers.py
@@ -1,2 +1,12 @@
 from django.contrib.auth.models import User
 from rest_framework import import serialisers
+
+class UserSerialiser(serialisers.ModelSerialisers):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
