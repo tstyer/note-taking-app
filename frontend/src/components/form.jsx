b820e429ed9206b54febb7bbf914b44554b46dbc
tstyer <travisstyer.ts@gmail.com>
2025-10-06 15:16:17 +0100

    Add imports to home.jsx

--- frontend/src/components/form.jsx
+++ frontend/src/components/form.jsx
@@ -3,7 +3,6 @@ import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/forms.css"
-import LoadingIndicator from "./LoadingIndicator";
 
 function Form({ route, method }) {
     const [username, setUsername] = useState("");
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,4 +1,4 @@
-import Form from "../components/form.jsx"
+import Form from "../components/form"
 
 function Login() {
     return <Form route="/api/token/" method="login" />
